a53403eb9284ff1a07a48c28080a963a909741b1
Jostyn G <Jostyn G@Jostyn.mshome.net>
2015-11-24 22:40:06 -0500

    ejercicios

---
+++ ejercicio3/src/ejercicio3/Descuento.java
@@ -0,0 +1,21 @@
+package ejercicio3;
+
+public class Descuento {
+
+	public static void main(String[] args) {
+		double descuento;
+		double totalCompras;
+		public Descuento(double descuentoParam, double totalComprasParam){
+			This.descuento=descuentoParam;
+					This.totalCompras=totalCompraParam;
+		}
+		public double gettotal(){
+			double total=totalCompras-totalCompras*descuento;
+			return;
+			//retunr totalCompras-totalCompras*descuento
+			
+		}
+
+	}
+
+}
