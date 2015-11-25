a9758d9c81e877b38911af6f6aa1c1c196972202
Jostyn G <Jostyn G@Jostyn.mshome.net>
2015-11-24 22:31:22 -0500

    ejercicio en clases

---
+++ ejercicio1/src/ejercicio1/circulo.java
@@ -0,0 +1,20 @@
+package ejercicio1;
+
+
+public class circulo {
+
+	public static void main(String[] args) {
+double radio;
+public void setRadio(double radioParam){
+	This.radio=radioParam;
+	
+}
+	public double getRadio(){
+		return This.radio;
+	}
+	public radio getArea(){
+		return.Marth.PI*radio*radio;
+	}
+	}
+
+}
